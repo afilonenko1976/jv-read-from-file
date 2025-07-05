6b83443ea89490a805cdf597a21df0e11781e4ae
Andrei Filonenko <a1.sankai@gmail.com>
2025-07-05 15:25:02 +0300

    added algorithm for loading strings from a file

---
+++ src/main/java/core/basesyntax/FileWorkDemo.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+
+public class FileWorkDemo {
+
+    public static void main(String[] args) {
+        FileWork fileWork = new FileWork();
+        String[] arr = fileWork.readFromFile("test22");
+    }
+}
