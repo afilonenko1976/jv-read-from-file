c8ababe8bae5dc0516c15f9b85988e05c1f6cd1b
Andrei Filonenko <a1.sankai@gmail.com>
2025-07-05 15:28:12 +0300

    delete demo class

--- src/main/java/core/basesyntax/FileWorkDemo.java
+++
@@ -1,9 +0,0 @@
-package core.basesyntax;
-
-public class FileWorkDemo {
-
-    public static void main(String[] args) {
-        FileWork fileWork = new FileWork();
-        String[] arr = fileWork.readFromFile("test22");
-    }
-}
